3a354b11909fbc6e325c6ebcde19bf15507428e6
insideplant <kami.10pearl@gmail.com>
2021-10-16 18:40:50 +0800

    display all operation logs

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import EventForm from './EventForm'
 import Events from './Events'
+import OperationLogs from './OperationLogs';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers'
-import operationLogs from '../reducers/operationLogs';
 
 const App= ()=> {
   const initialState= {
@@ -20,6 +20,7 @@ const App= ()=> {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   );
